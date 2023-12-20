66dd5b45445d8a4c4d0923bac2dee1cf88bc0d4f
chabidimas <chabidimas@gmail.com>
2023-12-20 02:14:52 +0100

    initial commit

---
+++ posts.js
@@ -0,0 +1,317 @@
+module.exports = [
+  {
+    "_id": "65823ca40c2821d863bb114a",
+    "index": 0,
+    "guid": "776c2f21-264d-4fac-be35-84eb8efc8b23",
+    "isActive": false,
+    "balance": "$2,841.16",
+    "picture": "http://placehold.it/32x32",
+    "age": 24,
+    "eyeColor": "green",
+    "name": "Summer Roth",
+    "gender": "female",
+    "company": "KONGENE",
+    "email": "summerroth@kongene.com",
+    "phone": "+1 (899) 594-2201",
+    "address": "716 Farragut Place, Vandiver, Puerto Rico, 1123",
+    "about": "Sit nostrud nulla laboris irure. Exercitation ipsum eu occaecat incididunt anim Lorem laborum sint. Laborum adipisicing dolor commodo ipsum sint officia amet sit elit. Est esse ea cupidatat officia deserunt sunt laboris qui nulla enim sit ut esse.\r\n",
+    "registered": "2018-09-11T04:57:23 -02:00",
+    "latitude": -17.407059,
+    "longitude": 49.445761,
+    "tags": [
+      "incididunt",
+      "labore",
+      "sint",
+      "aliqua",
+      "laboris",
+      "non",
+      "exercitation"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Long Norris"
+      },
+      {
+        "id": 1,
+        "name": "Roxanne Potter"
+      },
+      {
+        "id": 2,
+        "name": "Clarke Manning"
+      }
+    ],
+    "greeting": "Hello, Summer Roth! You have 7 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "65823ca46e380bb5059310a0",
+    "index": 1,
+    "guid": "6cacd64a-24ee-4ae8-8b82-eefbf71fc068",
+    "isActive": true,
+    "balance": "$2,375.52",
+    "picture": "http://placehold.it/32x32",
+    "age": 32,
+    "eyeColor": "blue",
+    "name": "Linda Parrish",
+    "gender": "female",
+    "company": "MIXERS",
+    "email": "lindaparrish@mixers.com",
+    "phone": "+1 (923) 545-2674",
+    "address": "500 Alton Place, Dawn, South Dakota, 9696",
+    "about": "Sunt sint qui occaecat et. Voluptate magna mollit Lorem nisi ad ex. Labore occaecat magna Lorem reprehenderit Lorem excepteur excepteur ipsum aute sit. Culpa nisi incididunt fugiat consequat consequat nisi. Enim laboris do labore nulla. Amet tempor fugiat ipsum consequat aute proident qui cillum.\r\n",
+    "registered": "2023-04-13T02:27:57 -02:00",
+    "latitude": -36.58169,
+    "longitude": 95.623818,
+    "tags": [
+      "sunt",
+      "in",
+      "nisi",
+      "laboris",
+      "occaecat",
+      "labore",
+      "ullamco"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Alissa Collier"
+      },
+      {
+        "id": 1,
+        "name": "Pennington Dickerson"
+      },
+      {
+        "id": 2,
+        "name": "Cecile Cooley"
+      }
+    ],
+    "greeting": "Hello, Linda Parrish! You have 6 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "65823ca4687ae557c15a75af",
+    "index": 2,
+    "guid": "fd9b12a5-a305-462f-89be-7efce662a7e2",
+    "isActive": true,
+    "balance": "$3,178.74",
+    "picture": "http://placehold.it/32x32",
+    "age": 25,
+    "eyeColor": "green",
+    "name": "Nieves Navarro",
+    "gender": "male",
+    "company": "BLUEGRAIN",
+    "email": "nievesnavarro@bluegrain.com",
+    "phone": "+1 (951) 407-2623",
+    "address": "981 Rewe Street, Snowville, Maryland, 1428",
+    "about": "Adipisicing qui ut aliqua non voluptate consectetur irure eiusmod sint proident proident. Aliquip aliqua in do minim consequat in qui sint adipisicing adipisicing incididunt. Dolore proident ad elit cillum consequat velit eiusmod irure cillum amet nisi. Adipisicing eiusmod proident est eu aliquip amet labore. Consectetur in sunt incididunt nulla proident ut dolore excepteur laboris sint aliqua sit quis aliquip. Anim deserunt elit cillum elit Lorem sint voluptate non eiusmod mollit cupidatat ea minim. Reprehenderit cupidatat proident nisi aliquip cupidatat proident in consequat officia.\r\n",
+    "registered": "2020-03-03T09:19:16 -01:00",
+    "latitude": -79.372267,
+    "longitude": 77.581507,
+    "tags": [
+      "reprehenderit",
+      "ipsum",
+      "nisi",
+      "sunt",
+      "exercitation",
+      "ut",
+      "dolore"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Powell Faulkner"
+      },
+      {
+        "id": 1,
+        "name": "Gilda Vazquez"
+      },
+      {
+        "id": 2,
+        "name": "Rhodes West"
+      }
+    ],
+    "greeting": "Hello, Nieves Navarro! You have 3 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "65823ca49c854ad5875ef787",
+    "index": 3,
+    "guid": "0bccc639-77db-4039-a327-0b0a98db1a07",
+    "isActive": true,
+    "balance": "$2,458.26",
+    "picture": "http://placehold.it/32x32",
+    "age": 33,
+    "eyeColor": "blue",
+    "name": "Rosalie Armstrong",
+    "gender": "female",
+    "company": "ICOLOGY",
+    "email": "rosaliearmstrong@icology.com",
+    "phone": "+1 (819) 539-2993",
+    "address": "652 Erasmus Street, Tioga, Vermont, 4994",
+    "about": "Laborum deserunt sint officia labore culpa exercitation proident voluptate tempor anim sit aliqua enim do. Cupidatat aute minim commodo duis quis aliqua sint excepteur. Eu id ut cillum aliqua. Velit proident nulla minim cupidatat non ea nostrud pariatur non magna veniam irure magna minim. Amet id fugiat laboris consectetur est sint Lorem commodo fugiat tempor ullamco ipsum proident ex. Voluptate id cupidatat consequat dolor qui id laboris laboris. Anim dolor laboris voluptate aliquip officia laborum aute ea.\r\n",
+    "registered": "2015-05-31T09:18:36 -02:00",
+    "latitude": -88.064017,
+    "longitude": 149.906521,
+    "tags": [
+      "cupidatat",
+      "deserunt",
+      "ut",
+      "consectetur",
+      "minim",
+      "laborum",
+      "veniam"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Glenn Larsen"
+      },
+      {
+        "id": 1,
+        "name": "Levine Pratt"
+      },
+      {
+        "id": 2,
+        "name": "Claire Patton"
+      }
+    ],
+    "greeting": "Hello, Rosalie Armstrong! You have 9 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "65823ca4ad0954dece8c24b9",
+    "index": 4,
+    "guid": "edb07c95-6257-4a85-b25d-57f1eab706f9",
+    "isActive": true,
+    "balance": "$3,667.36",
+    "picture": "http://placehold.it/32x32",
+    "age": 30,
+    "eyeColor": "green",
+    "name": "Torres Cunningham",
+    "gender": "male",
+    "company": "ESSENSIA",
+    "email": "torrescunningham@essensia.com",
+    "phone": "+1 (928) 433-2470",
+    "address": "140 Surf Avenue, Nicholson, South Carolina, 740",
+    "about": "Labore enim amet consequat sunt ullamco et quis qui mollit cupidatat ipsum sit. Lorem et aliquip enim ullamco. Dolor ut ipsum officia irure.\r\n",
+    "registered": "2022-10-09T02:21:04 -02:00",
+    "latitude": 50.586305,
+    "longitude": -46.265168,
+    "tags": [
+      "eu",
+      "minim",
+      "sunt",
+      "dolor",
+      "in",
+      "et",
+      "exercitation"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Jill Shaw"
+      },
+      {
+        "id": 1,
+        "name": "Black Rose"
+      },
+      {
+        "id": 2,
+        "name": "Adkins Patrick"
+      }
+    ],
+    "greeting": "Hello, Torres Cunningham! You have 2 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "65823ca40ea9ab8bc3cc0fb4",
+    "index": 5,
+    "guid": "accebe63-49c8-4787-b070-4bc9af8bfbcb",
+    "isActive": true,
+    "balance": "$3,043.58",
+    "picture": "http://placehold.it/32x32",
+    "age": 30,
+    "eyeColor": "green",
+    "name": "Peterson Reyes",
+    "gender": "male",
+    "company": "TEMORAK",
+    "email": "petersonreyes@temorak.com",
+    "phone": "+1 (989) 472-2539",
+    "address": "220 Hudson Avenue, Gordon, Virginia, 8685",
+    "about": "Fugiat velit in duis cupidatat minim dolore. Consectetur consequat anim velit ipsum ad excepteur sunt veniam nisi ad nostrud ex nulla. Et consequat in reprehenderit tempor sit Lorem est mollit. Consequat incididunt velit nisi aute laboris irure laboris fugiat.\r\n",
+    "registered": "2019-04-17T08:12:37 -02:00",
+    "latitude": 49.019842,
+    "longitude": -66.084435,
+    "tags": [
+      "ad",
+      "occaecat",
+      "culpa",
+      "consequat",
+      "veniam",
+      "ea",
+      "nisi"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Duran Pittman"
+      },
+      {
+        "id": 1,
+        "name": "Christina Cotton"
+      },
+      {
+        "id": 2,
+        "name": "Janis Reeves"
+      }
+    ],
+    "greeting": "Hello, Peterson Reyes! You have 3 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "65823ca489eb23453a572476",
+    "index": 6,
+    "guid": "4c70fe6f-1f44-4e5d-b1d0-b155de9cd190",
+    "isActive": false,
+    "balance": "$2,880.91",
+    "picture": "http://placehold.it/32x32",
+    "age": 21,
+    "eyeColor": "brown",
+    "name": "Rosanna Zamora",
+    "gender": "female",
+    "company": "ZAPHIRE",
+    "email": "rosannazamora@zaphire.com",
+    "phone": "+1 (865) 461-2831",
+    "address": "298 Harrison Place, Groveville, Connecticut, 4386",
+    "about": "Cillum veniam sit commodo ex et. Nisi aliquip do pariatur cupidatat anim labore aliquip elit in cupidatat Lorem proident exercitation. Elit minim esse aute ipsum aliqua culpa ad ea id. Cupidatat in cupidatat reprehenderit minim. Minim incididunt sint minim proident nulla magna minim eu fugiat duis. Adipisicing exercitation culpa velit elit laborum eiusmod exercitation duis amet cillum.\r\n",
+    "registered": "2017-06-11T03:28:56 -02:00",
+    "latitude": 70.126669,
+    "longitude": -23.881539,
+    "tags": [
+      "id",
+      "adipisicing",
+      "incididunt",
+      "culpa",
+      "qui",
+      "consequat",
+      "ullamco"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Penelope Sweet"
+      },
+      {
+        "id": 1,
+        "name": "Murray Rogers"
+      },
+      {
+        "id": 2,
+        "name": "Rosalind Howell"
+      }
+    ],
+    "greeting": "Hello, Rosanna Zamora! You have 3 unread messages.",
+    "favoriteFruit": "strawberry"
+  }
+]
